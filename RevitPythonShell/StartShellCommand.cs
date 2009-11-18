058f1e4a8c413475027291df2dd044942f1f39fe
dthomas.ch <dthomas.ch@dacc7128-d41e-11de-9aee-2b7272db14c4>
2009-11-18 10:07:27 +0000

    removed hardwired search paths and placed them in RevitPythonShell.XML

--- RevitPythonShell/StartShellCommand.cs
+++ RevitPythonShell/StartShellCommand.cs
@@ -2,19 +2,21 @@
 
 namespace RevitPythonShell
 {
-    class StartShellCommand: IExternalCommand
+    /// <summary>
+    /// An object of this class is instantiated every time the user clicks on the
+    /// button for opening the shell.
+    /// </summary>
+    public class StartShellCommand: IExternalCommand
     {
-        private static ScriptInput _gui;
-
         /// <summary>
         /// Open a window to let the user enter python code.
         /// </summary>
         /// <returns></returns>
         public IExternalCommand.Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
-        {
-            _gui = new ScriptInput(commandData.Application);
-            _gui.Show();
-            _gui.BringToFront();
+        {            
+            var gui = new ScriptInput(commandData.Application);
+            gui.Show();
+            gui.BringToFront();
             return IExternalCommand.Result.Succeeded;
         }
     }
