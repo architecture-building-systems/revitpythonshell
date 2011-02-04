3703ee173201e2e5b4eda9877c8ed0f52e90148e
dthomas.ch <dthomas.ch@dacc7128-d41e-11de-9aee-2b7272db14c4>
2011-02-04 10:29:12 +0000

    started adding a replacement project for the IronTextBox.

---
+++ trunk/IronPythonConsoleControl/OutputSpan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronPythonConsoleControl
+{
+    /// <summary>
+    /// Maintains the content and visual representation of a piece of chunk
+    /// of text. For wraping, this text must be splittable, by character count.
+    /// </summary>
+    public class OutputSpan
+    {
+    }
+}
