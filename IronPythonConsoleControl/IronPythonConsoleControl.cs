526b100ccc1b626f8ded9c561bcb5ace285ebb19
dthomas.ch <dthomas.ch@dacc7128-d41e-11de-9aee-2b7272db14c4>
2011-02-04 10:29:12 +0000

    started adding a replacement project for the IronTextBox.

---
+++ IronPythonConsoleControl/IronPythonConsoleControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Scripting.Hosting.Shell;
+using System.IO;
+
+namespace IronPythonConsoleControl
+{
+    /// <summary>
+    /// The IronPythonConsoleControl is a reusable control for implementing the IConsole
+    /// interface used by IronPython.
+    /// </summary>
+    public class IronPythonConsoleControl: Control, IConsole
+    {
+        private TextWriter _stderr;
+        private TextWriter _stdout;
+        private List<OutputLine> _lines; // the actual output, before rendering
+
+#region IConsole memebers
+        public TextWriter ErrorOutput { get { return _stderr; } set { _stderr = value; } }
+        public TextWriter Output { get { return _stdout; } set { _stdout = value; } }
+
+        /// <summary>
+        /// Blocks until the user has entered a line in the console.
+        /// </summary>
+        public string ReadLine(int autoIndentSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Write some text to the output. For now, we ignore style, but
+        /// we will be revisiting that soon!
+        /// </summary>
+        public void Write(string text, Style style)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteLine(string text, Style style)
+        {
+            Write(Text + Environment.NewLine, style);
+        }
+
+        public void WriteLine()
+        {
+            Write(Environment.NewLine, Style.Out);
+        }
+ 
+#endregion IConsole members
+    }
+}
---
+++ IronPythonConsoleControl/OutputLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronPythonConsoleControl
+{
+    /// <summary>
+    /// A line of output. Caches the rendering of this line for a given 
+    /// console width.
+    /// </summary>
+    public class OutputLine
+    {
+        private List<OutputSpan> _spans; // a list of writes (with possibly different 
+    }
+}
