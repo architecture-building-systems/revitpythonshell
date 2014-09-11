2b71d5edb71182100fa88ea82c421a3930f8fc51
dthomas.ch@gmail.com <dthomas.ch@gmail.com@dacc7128-d41e-11de-9aee-2b7272db14c4>
2014-09-11 12:26:09 +0000

    fixed a typo in a comment in startup.py

--- trunk/RevitPythonShell/startup.py
+++ trunk/RevitPythonShell/startup.py
@@ -2,7 +2,7 @@
 try:
     # add your code here
     # ...
-    __window__.Close()  # closes the window so 
+    __window__.Close()  # closes the window 
 except:
     import traceback       # note: add a python27 library to your search path first!
     traceback.print_exc()  # helps you debug when things go wrong
