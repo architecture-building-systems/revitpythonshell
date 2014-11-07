606c98f9af16a44daf0981d9323d8b992878bfd1
dthomas.ch@gmail.com <dthomas.ch@gmail.com@dacc7128-d41e-11de-9aee-2b7272db14c4>
2014-11-07 09:27:12 +0000

    proposed fix for __window__.Close() not working in the non-modal interactive shell

--- trunk/RevitPythonShell/init.py
+++ trunk/RevitPythonShell/init.py
@@ -16,3 +16,13 @@ def alert(msg):
 def quit():
     __window__.Close()
 exit = quit
+
+# a fix for the __window__.Close() bug introduced with the non-modal console
+class WindowWrapper(object):
+    def __init__(self, win):
+        self.win = win
+    def Close(self):
+        self.win.Dispatcher.Invoke(lambda *_: self.win.Close())
+    def __getattr__(self, name):
+        return getattr(self.win, name)
+ __window__ = WindowWrapper(__window__)
