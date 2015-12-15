5695d358ac9f97dd38dda80a39823ee7a9f862d9
daren-thomas <dthomas.ch@gmail.com>
2015-12-15 14:42:20 +0100

    still no improvement
    seems like a x64 / x86 situation thing?

--- RevitPythonShell/IronPythonConsoleCommand.cs
+++ RevitPythonShell/IronPythonConsoleCommand.cs
@@ -27,58 +27,8 @@ namespace RevitPythonShell
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {            
             var messageCopy = message;
-            var gui = new IronPythonConsole();
-            gui.consoleControl.WithHost((host) =>
-            {
-                // now that the console is created and initialized, the script scope should
-                // be accessible...
-                //new ScriptExecutor(RevitPythonShellApplication.GetConfig(), commandData, messageCopy, elements)
-                //    .SetupEnvironment(host.Engine, host.Console.ScriptScope);
-
-                //host.Console.ScriptScope.SetVariable("__window__", gui);
-
-                // run the initscript
-                //var initScript = RevitPythonShellApplication.GetInitScript();
-                //if (initScript != null)
-                //{
-                //    var scriptSource = host.Engine.CreateScriptSourceFromString(initScript, SourceCodeKind.Statements);
-                //    scriptSource.Execute(host.Console.ScriptScope);
-                //}                
-            });
-
-            var dispatcher = Dispatcher.FromThread(Thread.CurrentThread);
-            gui.consoleControl.WithHost((host) =>
-            {                
-                /*host.Console.SetCommandDispatcher((command) =>
-                {
-                    if (command != null)
-                    {
-                        // Slightly involved form to enable keyboard interrupt to work.
-                        var executing = true;
-                        var operation = dispatcher.BeginInvoke(DispatcherPriority.Normal, command);
-                        while (executing)
-                        {
-                            if (operation.Status != DispatcherOperationStatus.Completed)
-                                operation.Wait(TimeSpan.FromSeconds(1));
-                            if (operation.Status == DispatcherOperationStatus.Completed)
-                                executing = false;
-                        }
-                    }                 
-                });*/
-                /*host.Editor.SetCompletionDispatcher((command) =>
-                {
-                    var executing = true;
-                    var operation = dispatcher.BeginInvoke(DispatcherPriority.Normal, command);
-                    while (executing)
-                    {
-                        if (operation.Status != DispatcherOperationStatus.Completed)
-                            operation.Wait(TimeSpan.FromSeconds(1));
-                        if (operation.Status == DispatcherOperationStatus.Completed)
-                            executing = false;
-                    }
-                });*/
-            });
-            gui.ShowDialog();
+            var gui = new IronPythonConsole();            
+            gui.Show();
             return Result.Succeeded;
         }
     }    
