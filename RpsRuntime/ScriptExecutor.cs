b231e2aa266305edc71f91a022bf376b2af2d708
Daren Thomas <thomas@arch.ethz.ch>
2016-10-24 15:22:04 +0200

    created setup for PR #42

--- RpsRuntime/ScriptExecutor.cs
+++ RpsRuntime/ScriptExecutor.cs
@@ -75,7 +75,7 @@ namespace RevitPythonShell.RpsRuntime
                 scope.SetVariable("__window__", scriptOutput);
                 scope.SetVariable("__file__", sourcePath);
 
-                //Add script directory address to sys search paths
+                // Add script directory address to sys search paths
                 var path = engine.GetSearchPaths();
                 path.Add(System.IO.Path.GetDirectoryName(sourcePath));
                 engine.SetSearchPaths(path);
