31afc59defa5417bbbe50aff0188f0157bb41669
Ehsan Iran-Nejad <eirannejad@gmail.com>
2018-03-06 11:59:13 -0800

    Fixed RPS Versioning for 2018 and onward

--- RevitPythonShell/Properties/AssemblyInfo.cs
+++ RevitPythonShell/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RevitPythonShell")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.0.0.0")]
-[assembly: AssemblyFileVersion("2017.0.0.0")]
+[assembly: AssemblyVersion("2018.1.0.0")]
+[assembly: AssemblyFileVersion("2018.1.0.0")]
