10b00bb10fe123e9d689920c14777c74e0501052
Roman Golev <67314258+romangolev@users.noreply.github.com>
2025-03-25 17:05:43 +0100

    fix: restore build file

--- build/Build.Compile.cs
+++ build/Build.Compile.cs
@@ -1,7 +1,6 @@
 ﻿using Nuke.Common;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.MSBuild;
-using Serilog;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 
 internal partial class Build
@@ -13,24 +12,13 @@ internal partial class Build
              var configurations = GetConfigurations(BuildConfiguration, InstallerConfiguration);
              configurations.ForEach(configuration =>
              {
-                 Log.Debug("Building configuration: {configuration}", configuration);
-                 Log.Information("Building configuration: {configuration}", configuration);
-                 try
-                 {
-                     MSBuild(s => s
-                         .SetTargets("Rebuild")
-                         .SetProcessToolPath(MsBuildPath.Value)
-                         .SetConfiguration(configuration)
-                         //.SetVerbosity(MSBuildVerbosity.Diagnostic) // Increased verbosity
-                         .DisableNodeReuse()
-                         .EnableRestore());
-                 }
-                 catch (ProcessException ex)
-                 {
-                     Log.Error("MSBuild failed with an exception");
-                     Log.Error(ex, ex.Message);
-                     throw;
-                 }
+                 MSBuild(s => s
+                    .SetTargets("Rebuild")
+                    .SetProcessToolPath(MsBuildPath.Value)
+                    .SetConfiguration(configuration)
+                    .SetVerbosity(MSBuildVerbosity.Minimal)
+                    .DisableNodeReuse()
+                    .EnableRestore());
              });
          });
 }
